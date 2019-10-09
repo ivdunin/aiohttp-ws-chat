1883e7a66011e200c8056b28fdda15cc0413a04e
idunin <ilya.mirea@gmail.com>
2019-10-09 20:51:52 +0300

    Comments added;

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 # Author Dunin Ilya.
 """ Module description """
 
+import aiofiles
 from aiohttp import web, WSMsgType, WSCloseCode
 from aio_pika import connect_robust, Message
 from asyncio import get_event_loop
 from json import loads, dumps
+from json import decoder
 from logging import getLogger
+from pathlib import Path
 
 from utils import setup_logger
 from config import RMQ_CHANNEL_NAME, RMQ_CONN_STR
 
+BASE_DIR = Path(__file__).parent.absolute()
 logger = getLogger(__name__)
 setup_logger(logger)
 
@@ -30,11 +34,24 @@ class Application(web.Application):
         self.on_shutdown.append(self.cleanup_ws_conns)
 
     def setup_routes(self):
+        """ Setup Application routes """
         self.add_routes(
-            [web.get('/ws', self.ws_handler)]
+            [web.get('/ws', self.ws_handler),
+             web.get('/chat', self.js_chat)],
         )
 
+    async def js_chat(self, request):
+        async with aiofiles.open(BASE_DIR / "index.html", mode="r") as f:
+            page = await f.read()
+
+        return web.Response(text=page, content_type='text/html', charset='utf-8')
+
     async def cleanup_ws_conns(self, app):
+        """ Clean up all connection, send GOING AWAY signal for all connected clients
+
+        :param app:
+        :return:
+        """
         logger.info('Clean up connections...')
         for uid, ws in self.ws_connections.items():
             logger.info(f'Cleanup connection for user: {uid}')
@@ -44,6 +61,7 @@ class Application(web.Application):
             )
 
     async def ws_handler(self, request):
+        """ Process all incoming WS connections """
         ws = web.WebSocketResponse()
         await ws.prepare(request)
 
@@ -61,8 +79,11 @@ class Application(web.Application):
                 if msg.data == 'close':
                     await ws.close()
                 else:
-                    data = loads(msg.data)
-                    await self.publish_message(data.get('id'), data.get('msg'))
+                    try:
+                        data = loads(msg.data)
+                        await self.publish_message(data.get('id'), data.get('msg'))
+                    except decoder.JSONDecodeError:
+                        logger.warning('Incorrect message')
             elif msg.type == WSMsgType.ERROR:
                 logger.error('ws connection closed with exception %s', ws.exception())
 
@@ -71,6 +92,7 @@ class Application(web.Application):
         return ws
 
     async def listener(self, app):
+        """ RMQ listener """
         connection = await connect_robust(RMQ_CONN_STR, loop=self.aio_loop)
         channel = await connection.channel()
         await channel.set_qos(prefetch_count=100)
@@ -79,6 +101,7 @@ class Application(web.Application):
         return connection
 
     async def publish_message(self, uid, message):
+        """ Add message to RMQ """
         msg = {'id': str(uid), 'msg': message}
         connection = await connect_robust(RMQ_CONN_STR, loop=self.aio_loop)
         async with connection:
@@ -91,6 +114,7 @@ class Application(web.Application):
             )
 
     async def process_message(self, message):
+        """ Process incoming RMQ messages """
         async with message.process():
             data = loads(message.body)
             user_id = data.get('id')
@@ -108,10 +132,12 @@ class Application(web.Application):
                 await ws.send_str(msg)
 
     async def start_rmq(self, app):
+        """ Start RMQ listener """
         logger.info('Init Rabbit MQ')
         self['rmq'] = self.aio_loop.create_task(self.listener(self))
 
     async def stop_rmp(self, app):
+        """ Stop RMQ listener """
         logger.info('Stop Rabbit MQ')
         self['rmq'].cancel()
         await self['rmq']
